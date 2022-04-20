4ac3fa647706066812bc97ac1568e84e9fc13364
eriktja <erik.jarem@gmail.com>
2022-04-20 14:33:11 +0200

    game loop

--- TheGameEngine/src/main/java/Engine/Window/Game.java
+++ TheGameEngine/src/main/java/Engine/Window/Game.java
@@ -1,6 +1,7 @@
 package Engine.Window;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import java.io.Serial;
 
 public class Game extends Canvas implements Runnable{
@@ -9,17 +10,74 @@ public class Game extends Canvas implements Runnable{
 
     public static final int width = 640, height = width/ 12 * 9;
 
+    private Thread thread;
+    private boolean running = false;
+
     public Game() {
         new Window(width, height, "hello", this);
     }
 
     public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
 
+    public synchronized void stop(){
+        try {
+            thread.join();
+            running = false;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public synchronized void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1){
+                tick();
+                delta--;
+            }
+            if(running){
+                render();
+            }
+            frames++;
 
+            if(System.currentTimeMillis() - timer > 1000){
+                timer = System.currentTimeMillis();
+                System.out.println("FPS " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.PINK);
+        g.fillRect(0,0,width, height);
+        g.dispose();
+        bs.show();
     }
 
 }
