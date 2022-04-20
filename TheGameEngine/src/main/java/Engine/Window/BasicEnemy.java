e45891d2a3ac7f4199c4a5197325718b469fe32d
eriktja <erik.jarem@gmail.com>
2022-04-20 18:14:45 +0200

    path testing

--- TheGameEngine/src/main/java/Engine/Window/BasicEnemy.java
+++ TheGameEngine/src/main/java/Engine/Window/BasicEnemy.java
@@ -1,9 +1,11 @@
 package Engine.Window;
 
 import java.awt.*;
+import java.util.Random;
 
 public class BasicEnemy extends GameObject {
-
+    private static int counter = 0;
+    private Random r = new Random();
     private Handler handler;
     private int height = 16;
     private int width = 16;
@@ -12,8 +14,12 @@ public class BasicEnemy extends GameObject {
         super(x, y, id);
         this.handler = handler;
 
-        velX = 15;
-        velY = 15;
+        counter++;
+
+        velX = 5;
+        velY = 5;
+        if(counter % 2 == 0) velX *= -2;
+        if(counter % 3 == 0) velY *= -2;
     }
 
     @Override
@@ -21,10 +27,10 @@ public class BasicEnemy extends GameObject {
         x += velX;
         y += velY;
 
-        if(y <= 0 || y >= Game.height) velY *= -1;
+        if(y <= 0 || y >= Game.height) velY *=  -1;
         if(x <= 0 || x >= Game.width) velX *= -1;
 
-        handler.addObject(new Trail(x, y, ID.Trail, width, height, Color.RED, handler, 0.01f));
+        handler.addObject(new Trail(x, y, ID.Trail, width, height, Color.RED, handler, 0.1f));
     }
 
     @Override
